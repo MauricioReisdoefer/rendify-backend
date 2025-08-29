9ea042ff66edf6e1217c68630244f94ff83f09fe
Mauricio Reisdoefer <mauricio.reisdoefer2009@gmail.com>
2025-08-28 21:59:57 -0300

    FEAT: Creating user controller

---
+++ Controllers/stock_controller.py
@@ -0,0 +1,47 @@
+from flask import request, jsonify
+from Extras.db import db
+from Models.stock_model import StockModel
+from twelvedata import TDClient
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+TD_API_KEY = os.getenv("TWELVEDATA_API_KEY")
+td = TDClient(apikey=TD_API_KEY)
+
+def update_stock_price():
+    """
+    Atualiza ou cria Stock no banco usando TwelveData
+    """
+    data = request.json
+    symbol = data.get("symbol")
+    exchange = data.get("exchange")
+    currency = data.get("currency", "USD")
+
+    if not symbol or not exchange:
+        return jsonify({"error": "Symbol and exchange are required"}), 400
+    try:
+        ts = td.price(symbol=symbol.upper()).as_json()
+        price = float(ts["price"])
+    except Exception as e:
+        return jsonify({"error": str(e)}), 400
+
+    # Verifica se já existe
+    stock = StockModel.query.filter_by(symbol=symbol.upper()).first()
+    if stock:
+        stock.price = price
+        stock.exchange = exchange
+        stock.currency = currency
+    else:
+        stock = StockModel(symbol=symbol, exchange=exchange, currency=currency, price=price)
+        db.session.add(stock)
+
+    db.session.commit()
+    return jsonify(stock.json()), 200
+
+def get_stock(symbol):
+    stock = StockModel.query.filter_by(symbol=symbol.upper()).first()
+    if not stock:
+        return jsonify({"error": "Stock not found"}), 404
+    return jsonify(stock.json()), 200
