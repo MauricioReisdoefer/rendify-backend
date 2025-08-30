459ab4cbedfd6f36f1813d8b8055d251a604705d
Mauricio Reisdoefer <mauricio.reisdoefer2009@gmail.com>
2025-08-29 21:08:19 -0300

    FEAT: Creating simulator routes

---
+++ Routes/simulator_routes.py
@@ -0,0 +1,14 @@
+from flask import Blueprint
+from Controllers.simulator_controller import (
+    add_or_update_simulator,
+    get_simulator,
+    get_all_simulators,
+    delete_simulator,
+)
+
+simulator_bp = Blueprint("simulator_bp", __name__, url_prefix="/simulator")
+
+simulator_bp.route("/add", methods=["POST"])(add_or_update_simulator)
+simulator_bp.route("/get/<string:symbol>", methods=["GET"])(get_simulator)
+simulator_bp.route("/", methods=["GET"])(get_all_simulators)
+simulator_bp.route("/delete/<string:symbol>", methods=["DELETE"])(delete_simulator)
