5cf7d63ca02b17a55653ba3dd0ecb5079674b0e7
Mauricio Reisdoefer <mauricio.reisdoefer2009@gmail.com>
2025-08-29 21:14:13 -0300

    FEAT: Register blueprints in run.py and correction in Extras.db imports

--- run.py
+++ run.py
@@ -1,24 +1,29 @@
-from Routes import user_route
 from flask import Flask
-from Extras import db
-# from Routes import user_route, stock_routes, watchlist_routes
-
+from Extras.db import db
 app = Flask(__name__)
-# app.register_blueprint(user_route.user_bp, url_prefix='/user')
-# app.register_blueprint(stock_routes.stock_bp, url_prefix='/stock')
-# app.register_blueprint(watchlist_routes.watchlist_bp, url_prefix='/watch')
+
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'  
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db.db.init_app(app)
+from Routes.user_routes import user_bp
+from Routes.stock_routes import stock_bp
+from Routes.watchlist_routes import watchlist_bp
+from Routes.simulator_routes import simulator_bp
+
+app.register_blueprint(user_bp)
+app.register_blueprint(stock_bp)
+app.register_blueprint(watchlist_bp)
+app.register_blueprint(simulator_bp)
+
+db.init_app(app)
 
 from flask_cors import CORS
 CORS(app)
 
 
 with app.app_context():
-    db.db.create_all()
+    db.create_all()
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=True)
