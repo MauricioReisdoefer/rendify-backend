0d44eb58406954a46471a732a3eaf10d602d0870
Mauricio Reisdoefer <mauricio.reisdoefer2009@gmail.com>
2025-09-01 20:21:48 -0300

    FEAT: Adding db to github

--- Routes/stock_routes.py
+++ Routes/stock_routes.py
@@ -4,4 +4,4 @@ from Controllers.stock_controller import update_stock_price, get_stock
 stock_bp = Blueprint("stock_bp", __name__, url_prefix="/stock")
 
 stock_bp.route("/update", methods=["POST"])(update_stock_price)
-stock_bp.route("/<string:symbol>", methods=["GET"])(get_stock)
+stock_bp.route("/search/<string:symbol>", methods=["GET"])(get_stock)
